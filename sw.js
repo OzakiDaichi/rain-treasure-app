684980644f5eaf8d76139b4b88956f3e98192d56
OzakiDaichi <ozaki.daichi.72u@st.kyoto-u.ac.jp>
2026-09-02 23:21:35 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='rain-v3-4-growth';
+const C='rain-v3-4-no-garden-copy';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','manifest.webmanifest'])));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).catch(()=>caches.match('./')));return;}e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
